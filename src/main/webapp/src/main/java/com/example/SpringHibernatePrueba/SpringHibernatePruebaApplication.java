d6f31440bd3691ac08280aea2ca1df6f7475c8f7
slidel-bk <na631295@gmail.com>
2022-11-18 16:56:36 -0600

    second commit

---
+++ src/main/webapp/src/main/java/com/example/SpringHibernatePrueba/SpringHibernatePruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringHibernatePrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHibernatePruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHibernatePruebaApplication.class, args);
+	}
+
+}

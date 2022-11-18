ed1bf2793ae5d0a2286daf2619a50ab64800f690
slidel-bk <na631295@gmail.com>
2022-11-18 16:55:14 -0600

    first commit

---
+++ src/test/java/com/example/SpringHibernatePrueba/SpringHibernatePruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringHibernatePrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHibernatePruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
